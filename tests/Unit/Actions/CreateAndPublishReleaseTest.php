151153a9dc99625a7a597431c98b7f4f080d28e4
Ashley Gibson <ashley@nosegraze.com>
2022-04-29 19:55:37 +0100

    Add test. #4

---
+++ tests/Unit/Actions/CreateAndPublishReleaseTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * CreateAndPublishReleaseTest.php
+ *
+ * @package   edd-sl-releases
+ * @copyright Copyright (c) 2022, Ashley Gibson
+ * @license   MIT
+ */
+
+namespace EddSlReleases\Tests\Feature\Actions;
+
+class CreateAndPublishReleaseTest
+{
+
+}
